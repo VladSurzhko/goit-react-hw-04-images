204ea666b3094e84ae022c2cc40bf60fc14e5414
VladSurzhko <surzhkovlad69@gmail.com>
2023-06-21 20:36:09 +0200

    eee

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -11,6 +11,9 @@ export const Overlay = styled.div`
   align-items: center;
   background-color: rgba(0, 0, 0, 0.8);
   z-index: 1200;
+  overflow:hidden;
+overflow-y:hidden;  /*для вертикального*/
+overflow-x:hidden;  /*для горизонтального*/
   
 `;
 
